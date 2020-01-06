307d246298384246721e80be1d2c209ec626aeb5
Vaidas <shibalba1@gmail.com>
2020-01-06 23:39:56 +0100

    cleanup

--- src/providers/oneprovider.com.js
+++ src/providers/oneprovider.com.js
@@ -5,18 +5,11 @@ const DomainFetch = searchDomain =>
   fetch("https://oneprovider.com/dedicated-servers/amsterdam-netherlands", {
     "credentials": "include",
     "headers": {
-      "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
-      "accept-language": "en-GB,en-US;q=0.9,en;q=0.8,nl;q=0.7,lt;q=0.6",
-      "cache-control": "max-age=0",
-      "if-modified-since": "Mon, 06 Jan 2020 21:05:04 GMT",
-      "if-none-match": "W/\"1578344704\"",
       "sec-fetch-mode": "navigate",
       "sec-fetch-site": "same-origin",
       "sec-fetch-user": "?1",
       "upgrade-insecure-requests": "1"
     },
-    "referrer": "https://oneprovider.com/dedicated-servers/dusseldorf-germany",
-    "referrerPolicy": "no-referrer-when-downgrade",
     "body": null,
     "method": "GET",
     "mode": "cors"
@@ -26,34 +19,57 @@ const DomainFetch = searchDomain =>
     const $ = cheerio.load(text);
     let servers = [];
 
-    $('body table > tbody > tr').map((i, $tr) => {
+    $('.drive-bundle-id').remove();
+    $('body table > tbody > tr').map((i, tr) => {
       let server = {};
+      const $tr = $(tr);
 
-      $($tr).find('td').map((j, $td) => {
+      $tr.find('td').map((j, td) => {
+        const $td = $(td);
+
         switch(j) {
           case 0:
-            server.location = $($td).find('a').text().replace(/\s+/g, ' ').trim();
+            server.location = {
+              city: $td.find('.field-location-name').text(),
+              country: $td.find('.field-location-country').text(),
+            };
             break;
           case 1:
-            server.cpu = $($td).text().replace(/\s+/g, ' ').trim();
+            server.cpu = {
+              name: $td.find('.field-cpu-name').text(),
+              freq: $td.find('.field-cpu-freq').text(),
+              cores: parseInt($td.find('.field-cpu-core').text().replace(' cores', '')),
+            };
             break;
           case 2:
-            server.ram = $($td).text().replace(/\s+/g, ' ').trim();
+            server.ram = {};
+            server.ram.version = $td.text().replace(/\s+/g, ' ').trim();
+            server.ram.amout = server.ram.version.replace(/\sDDR\d/g, '').split(' ');
+            server.ram.unit = server.ram.amout[1];
+            server.ram.amout = server.ram.amout[0];
+            server.ram.version = server.ram.version.match(/DDR\d/g)[0];
             break;
           case 3:
-            server.storage = $($td).text().replace(/\s+/g, ' ').trim();
+            server.storage = $td.text().replace(/\s+/g, ' ').trim();
+            server.storage = server.storage.split('or').map(s => s.trim()).filter(s => s !== '');
+            server.storage = server.storage.map(s => {
+              const match = s.match(/(?<amount>\d{1,}).\s(?<size>\d{1,})\s(?<unit>\w{2})\s\((?<type>\w+)\s(?<conn_type>\w+)\)/);
+              return match.groups;
+            });
+            server.storage = server.storage.map(s => ({ ...s }));
             break;
           case 4:
-            server.bandwidth = $($td).find('.number').text().replace(/\s+/g, ' ').trim();
+            server.bandwidth = $td.find('.number').text().replace(/\s+/g, ' ').trim();
             break;
           case 5:
-            server.ip = $($td).text().replace(/\s+/g, ' ').trim();
+            server.ip = parseInt($td.text().replace(/\s+/g, ' ').trim());
             break;
           case 6:
-            server.price = $($td).find('.currency-code-eur').text().replace(/\s+/g, ' ').trim();
+            server.price = $td.find('.currency-code-eur').text().replace(/\s+/g, ' ').trim();
+            server.price = server.price.split(' ').map(p => Math.round(parseFloat(p.replace('€', ''))));
             break;
           case 7:
-            server.availability = $($td).find('a').attr('data-tooltip').replace(/\s+/g, ' ').trim();
+            server.available = $td.find('a').attr('data-tooltip').replace(/\s+/g, ' ').trim() === 'In Stock';
             break;
         }
 
@@ -64,6 +80,6 @@ const DomainFetch = searchDomain =>
     return {
       servers,
     };
-  }).catch(e => console.log(provider, e));
+  }).catch(e => console.log(e));
 
 module.exports = DomainFetch;
--- src/search.js
+++ src/search.js
@@ -13,7 +13,6 @@ const availabilitySearch = (domain, filterProvider) => Promise.all(
   getSearchProviders(filterProvider).map(provider => {
     const startedAt = new Date();
 
-    //todo: deny if longer than 2 seconds
     return providerApis[provider](domain).then(data => ({
       ...data,
       meta: {
@@ -24,25 +23,6 @@ const availabilitySearch = (domain, filterProvider) => Promise.all(
   }
 ));
 
-const convertCurrency = async (dataArray = []) => {
-  const currencyRates = await getCurrencyRates();
-
-  return dataArray.map((data = {}) => ({
-    ...data,
-    currency: 'EUR',
-    price: data.price ? (currencyRates[data.currency] * data.price).toFixed(2) : null,
-    renewal: data.renewal ? (currencyRates[data.currency] * data.renewal).toFixed(2) : null,
-  })).map(data => ({
-    ...data,
-    meta: {
-      ...data.meta,
-      price2: (parseFloat(data.price) + parseFloat(data.renewal)).toFixed(2)
-    }
-  }));
-};
-
 module.exports = (domain, filterProvider) =>
   availabilitySearch(domain, filterProvider)
-    // .then(searchResults => searchResults.filter((searchResult = {}) => !!searchResult.domain))
-    // .then(convertCurrency)
 ;
