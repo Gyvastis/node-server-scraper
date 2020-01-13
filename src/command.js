20ef6c515d1bec4e9d6d11bddab99cf7d58d679c
Vaidas <shibalba1@gmail.com>
2020-01-13 22:10:28 +0100

    add output

--- src/command.js
+++ src/command.js
@@ -1,7 +1,16 @@
 const util = require('util');
+const fs = require('fs');
 const domainSearch = require('./search');
 
 domainSearch(
   process.argv[2]
-).then(data => console.log(data));
+).then(data => {
+  try {
+    fs.writeFileSync('./output/output.json', JSON.stringify(data, null, 4));
+  } catch(err) {
+    // An error occurred
+    console.error(err);
+  }
+  console.log(data)
+});
 // ).then(data => console.log(util.inspect(data, false, null, true)));
